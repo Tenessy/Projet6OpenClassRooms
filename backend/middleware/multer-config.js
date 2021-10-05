8b1c26d41fd8a7bb79f4cab4bd13e346e2b6e9b1
Tenessy <tendu35@hotmail.fr>
2021-10-05 21:03:59 +0200

    backend

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -11,9 +11,8 @@ const storage = multer.diskStorage({
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.fieldname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + '_' + Date.now() + '.' + extension);
+    callback(null, file.fieldname + '_' + Date.now() + '.' + extension);
   }
 });
 
